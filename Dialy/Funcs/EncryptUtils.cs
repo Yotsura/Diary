ade105deb60d11ada311aaacc0c96d2a258f079b
Yotsura <gxb65358@gmail.com>
2020-05-30 12:30:06 +0900

    コード整理

--- Dialy/Funcs/EncryptUtils.cs
+++ Dialy/Funcs/EncryptUtils.cs
@@ -17,20 +17,19 @@ namespace Dialy.Funcs
             {
                 var byteValue = Encoding.UTF8.GetBytes("0123456789ABCDEF" + value);
                 var encryptor = aes.CreateEncryptor();
-                var encryptValue = encryptor.TransformFinalBlock(byteValue, 0, byteValue.Length);
-                return Convert.ToBase64String(encryptValue);
+                var encryptedValue = encryptor.TransformFinalBlock(byteValue, 0, byteValue.Length);
+                return Convert.ToBase64String(encryptedValue);
             }
         }
 
-        internal static string AesDecrypt(string encryptValue)
+        internal static string AesDecrypt(string encryptedValue)
         {
             using (var aes = GetAesManaged())
             {
-                var byteValue = Convert.FromBase64String(encryptValue);
+                var byteValue = Convert.FromBase64String(encryptedValue);
                 var decryptor = aes.CreateDecryptor();
                 var decryptValue = decryptor.TransformFinalBlock(byteValue, 0, byteValue.Length);
-                var result = Encoding.UTF8.GetString(decryptValue, 16, decryptValue.Length - 16);
-                return result;
+                return Encoding.UTF8.GetString(decryptValue, 16, decryptValue.Length - 16);
             }
         }
 
@@ -44,14 +43,14 @@ namespace Dialy.Funcs
             };
             //IV初期ベクトルはあくまで同じ平文・鍵で別の暗号文を生成するためのもの。
             aes.IV = Encoding.UTF8.GetBytes(System.Web.Security.Membership.GeneratePassword(16, 0));
-            var encriptedKey = Environment.GetEnvironmentVariable("DiaryKey", EnvironmentVariableTarget.User);
-            if (string.IsNullOrEmpty(encriptedKey))
+            var encryptedKey = Environment.GetEnvironmentVariable("DiaryKey", EnvironmentVariableTarget.User);
+            if (string.IsNullOrEmpty(encryptedKey))
             {
                 //環境変数がなければ作成・設定する
-                encriptedKey = DpapiEncrypt(System.Web.Security.Membership.GeneratePassword(32, 0));
-                Environment.SetEnvironmentVariable("DiaryKey", encriptedKey, EnvironmentVariableTarget.User);
+                encryptedKey = DpapiEncrypt(System.Web.Security.Membership.GeneratePassword(32, 0));
+                Environment.SetEnvironmentVariable("DiaryKey", encryptedKey, EnvironmentVariableTarget.User);
             }
-            aes.Key = Encoding.UTF8.GetBytes(DpapiDecrypt(encriptedKey));
+            aes.Key = Encoding.UTF8.GetBytes(DpapiDecrypt(encryptedKey));
             aes.Padding = PaddingMode.PKCS7;
             return aes;
         }
@@ -65,10 +64,10 @@ namespace Dialy.Funcs
             return System.Convert.ToBase64String(encryptedData);
         }
 
-        private static string DpapiDecrypt(string encryptValue)
+        private static string DpapiDecrypt(string encryptedValue)
         {
             //文字列を暗号化されたデータに戻す
-            byte[] encryptedData = System.Convert.FromBase64String(encryptValue);
+            byte[] encryptedData = System.Convert.FromBase64String(encryptedValue);
             byte[] userData = ProtectedData.Unprotect(encryptedData, entropy, DataProtectionScope.CurrentUser);
             //復号化されたデータを文字列に変換
             return System.Text.Encoding.UTF8.GetString(userData);
