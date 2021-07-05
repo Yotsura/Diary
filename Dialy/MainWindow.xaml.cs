e0c3180435521b9610ad3ccf81a1d7cfea08db1d
Yotsura <gxb65358@gmail.com>
2021-07-05 10:48:56 +0900

    閉じる時にメモエリアの幅が０以上の場合はその時の幅を保存するように変更

--- Dialy/MainWindow.xaml.cs
+++ Dialy/MainWindow.xaml.cs
@@ -164,6 +164,8 @@ namespace Dialy
             }
             Settings.Default.MainWindowStat = new WindowStat { Height = this.Height, Width = this.Width, Left = this.Left, Top = this.Top };
             Settings.Default.FontSize = _mwvm.IndicateSize;
+            if (TaskAreaCol.Width.Value > 0)
+                Settings.Default.TaskAreaWidth = TaskAreaCol.Width;
             Settings.Default.TaskAreaValue = TaskArea.Text;
             Settings.Default.Save();
         }
